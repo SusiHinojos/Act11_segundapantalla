bcd4a1340fcef4622cf8c2abbea8e5445d3eafa9
a.23308051280248 <a.23308051280248@cbtis128.edu.mx>
2026-02-18 18:49:57 +0000

    ejemplo

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,130 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const JardinMaravillasApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class JardinMaravillasApp extends StatelessWidget {
+  const JardinMaravillasApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Jardín de las Maravillas',
+      theme: ThemeData(useMaterial3: true),
+      home: const AtraccionesPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class AtraccionesPage extends StatelessWidget {
+  const AtraccionesPage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  static const IconData attractions = IconData(0xe0b4, fontFamily: 'MaterialIcons');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    const Color purpleTheme = Colors.deepPurple;
+    
+    // Detectamos el ancho de la pantalla
+    double screenWidth = MediaQuery.of(context).size.width;
+    // Si es más ancho de 600px (Web/Tablet), ponemos 3 columnas, si no, 2.
+    int crossAxisCount = screenWidth > 600 ? 3 : 2;
+
+    final List<Map<String, String>> listaAtracciones = [
+      {'nombre': 'Rueda de la fortuna', 'img': 'https://raw.githubusercontent.com/SusiHinojos/imagenes_paraflutter_6J_Febrero_2026/refs/heads/main/rueda.jpg'},
+      {'nombre': 'Carrusel', 'img': 'https://raw.githubusercontent.com/SusiHinojos/imagenes_paraflutter_6J_Febrero_2026/refs/heads/main/carrusel.jpg'},
+      {'nombre': 'Tren del parque', 'img': 'https://raw.githubusercontent.com/SusiHinojos/imagenes_paraflutter_6J_Febrero_2026/refs/heads/main/tren.jpg'},
+      {'nombre': 'Barco vikingo', 'img': 'https://raw.githubusercontent.com/SusiHinojos/imagenes_paraflutter_6J_Febrero_2026/refs/heads/main/barco.jpg'},
+      {'nombre': 'Tazas giratorias', 'img': 'https://raw.githubusercontent.com/SusiHinojos/imagenes_paraflutter_6J_Febrero_2026/refs/heads/main/tazas.jpg'},
+      {'nombre': 'Sillas voladoras', 'img': 'https://raw.githubusercontent.com/SusiHinojos/imagenes_paraflutter_6J_Febrero_2026/refs/heads/main/sillas.jpg'},
+    ];
+
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: purpleTheme,
+        elevation: 0,
+        leading: const Icon(attractions, color: Colors.white, size: 28),
+        centerTitle: true, 
+        title: const Text(
+          'Jardín de las Maravillas',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          const SizedBox(height: 15),
+          const Text(
+            'Atracciones básicas',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: purpleTheme),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                // Aumentamos este valor para que el contenedor sea más bajito (compacto)
+                childAspectRatio: 1.1, 
+              ),
+              itemCount: listaAtracciones.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: purpleTheme, width: 1.5),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 5),
+                      Text(
+                        listaAtracciones[index]['nombre']!,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: purpleTheme),
+                      ),
+                      const SizedBox(height: 5),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 6, right: 6, bottom: 6),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              listaAtracciones[index]['img']!,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(top: BorderSide(color: purpleTheme, width: 2.0)),
+        ),
+        child: BottomAppBar(
+          color: Colors.white,
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(icon: const Icon(Icons.person, color: purpleTheme), onPressed: () {}),
+              IconButton(icon: const Icon(attractions, color: purpleTheme, size: 28), onPressed: () {}),
+              IconButton(icon: const Icon(Icons.home, color: purpleTheme), onPressed: () {}),
+              IconButton(icon: const Icon(Icons.confirmation_number, color: purpleTheme), onPressed: () {}),
+              IconButton(icon: const Icon(Icons.shopping_cart, color: purpleTheme), onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
